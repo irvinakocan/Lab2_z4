20f5111ac3e21c9a2e959b7e3e524ee8b1dff4c5
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-04 06:45:04 +0200

    Implementirana funkcija DaLiJePalindrom

--- Lab2_z4/Program.cs
+++ Lab2_z4/Program.cs
@@ -4,9 +4,46 @@ namespace Lab2_z4
 {
     class Program
     {
+        static bool DaLiJePalindrom(string s)
+        {
+            char[] pojedinacnaSlova = new char[s.Length];
+            int stvarnaDuzina = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsWhiteSpace(s[i]) && !char.IsPunctuation(s[i]))
+                {
+                    if (char.IsUpper(s[i]))
+                    {
+                        pojedinacnaSlova[stvarnaDuzina] = char.ToLower(s[i]);
+                        stvarnaDuzina++;
+                    }
+                    else
+                    {
+                        pojedinacnaSlova[stvarnaDuzina] = s[i];
+                        stvarnaDuzina++;
+                    }
+                }
+            }
+
+            bool palindrom = true;
+            int pomocna = stvarnaDuzina - 1;
+            for(int i = 0; i < stvarnaDuzina; i++)
+            {
+                if (pojedinacnaSlova[i] != pojedinacnaSlova[pomocna])
+                    palindrom = false;
+                pomocna--;
+            }
+
+            return palindrom;
+        }
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Unesite string: ");
+            string s = Console.ReadLine();
+            string ishod = DaLiJePalindrom(s) ? "jeste" : "nije";
+            Console.WriteLine("Uneseni string {0} palindrom.", ishod);
         }
     }
 }
